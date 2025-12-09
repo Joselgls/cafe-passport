96677ef29920760dc0d601d460e8dbf85ffe78f9
Joselgls <jlgj98@gmail.com>
2025-12-09 08:09:52 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,16 @@ export default function Passport() {
 }
 
 //export default function PassportPage() {
-  return (
-    <main
-      style={{
-        minHeight: "100vh",
-        padding: "2rem",
-        background: "#f3f1e7",
-      }}
-    >
-      <h1>Flat-White Passport</h1>
-      <p>Esta será la página del pasaporte con todos los cafés.</p>
-    </main>
-  );
-}
+//  return (
+//    <main
+//      style={{
+//        minHeight: "100vh",
+//        padding: "2rem",
+//        background: "#f3f1e7",
+//      }}
+//    >
+//      <h1>Flat-White Passport</h1>
+//      <p>Esta será la página del pasaporte con todos los cafés.</p>
+//    </main>
+//  );
+//}
